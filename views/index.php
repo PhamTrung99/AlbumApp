03e4ac0db714b6566c636adbc0ebcafd3e527303
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-08-21 11:06:29 +0700

    commit

---
+++ views/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+    <title>Album App</title>
+</head>
+
+<body>
+    <div class="container" style="margin: 10px">
+        <div class="row" style="height: 6rem;align-items: center;">
+            Banner
+        </div>
+        <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-2" style="text-align: end;"><a class="btn btn-primary" href="#" role="button">Tìm kiếm
+                </a></div>
+            <div class="col-md-2" style="text-align: end;"><a class="btn btn-primary" href="#" role="button">Thêm bài
+                    hát mới</a></div>
+            <div class="col-md-2" style="text-align: end;"> <a class="btn btn-primary" href="#" role="button">Thêm
+                    Album</a></div>
+        </div>
+        <div class="row" style="padding-top: 10px">
+            <div class="col-md-2">
+                <div class="row">
+                    <ul class="list-group" style="width: 300px;">
+                        <li class="list-group-item active">Các thể loại</li>
+                        <li class="list-group-item">Thể loại 1</li>
+                        <li class="list-group-item">Thể loại 2</li>
+                        <li class="list-group-item">Thể loại 3</li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <ul class="list-group" style="width: 300px; padding-top: 16px;">
+                        <li class="list-group-item active">Các ca sĩ/ Nhóm nhạc</li>
+                        <li class="list-group-item">Ca sĩ 1</li>
+                        <li class="list-group-item">Ca sĩ 2</li>
+                        <li class="list-group-item">Ca sĩ 3</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-8">Hello</div>
+            <div class="col-md-2">
+                <div class="row" style="flex-direction: row-reverse;">
+                    <ul class="list-group" style="width: 300px;">
+                        <li class="list-group-item active">Đăng nhập</li>
+                    </ul>
+                    <form style="font-size: 12px;">
+                        <div class="form-group" style="display: inline-flex;">
+                            <label for="userName">Tên Đăng nhập</label>
+                            <input type="email" class="form-control" id="userName">
+                        </div>
+                        <div class="form-group" style="display: inline-flex;">
+                            <label for="passWord">Mât Khẩu</label>
+                            <input type="password" class="form-control" id="passWord">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Đăng nhập</button>
+                    </form>
+                </div>
+                <div class="row" style="flex-direction: row-reverse;">
+                    <ul class="list-group" style="width: 300px; padding-top: 16px;">
+                        <li class="list-group-item active">Các ca sĩ/ Nhóm nhạc</li>
+                        <li class="list-group-item">Ca sĩ 1</li>
+                        <li class="list-group-item">Ca sĩ 2</li>
+                        <li class="list-group-item">Ca sĩ 3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="../assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
