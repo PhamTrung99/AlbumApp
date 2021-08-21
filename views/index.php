9adb46a5e67a09cbb4e21a2a105fcc099492d13b
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-08-21 11:20:18 +0700

    commit

--- views/index.php
+++ views/index.php
@@ -70,6 +70,14 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="" style="margin-left: 24rem;
+                            font-size: 22px;
+                            line-height: 73px;
+                            letter-spacing: 0.08em;
+                            font-weight: 200;
+                            font-style: normal;">1712848 - Phạm Quốc Trung</div>
+        </div>
     </div>
 
 
