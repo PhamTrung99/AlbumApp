f4df78a61d6f3714ffd9f4c7f65ee6f9f5d28ef2
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-08-22 09:19:28 +0700

    commit

--- assets/js/handleLoadWhenLogin.js
+++ assets/js/handleLoadWhenLogin.js
@@ -1,8 +1,9 @@
 function handleLoadWhenLogin() {
     var userName = document.cookie;
     if (userName != "") { 
-        document.getElementById("formLogin").innerHTML = `<div class="div" style="font-size:25px;">Chào ${userName} </div>
-    <button class="btn btn-outline-dark" onclick="handleLogOut()">Thoát</a>`
+        document.getElementById("formLogin").innerHTML = `<div class="div" style="font-size:25px;text-align: center">Chào ${userName} </div>
+        <div class="row" ><img src="../assets/image/avatar.jpeg" width="100px" height="100px"></div>
+        <button class="btn btn-outline-dark" onclick="handleLogOut()">Thoát</button>`
     } else {
       
     }
