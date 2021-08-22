89f5fcd98da5863db3376a24d25a3e3c14763ab5
PhamTrung99 <1712848@student.hcmus.edu.vn>
2021-08-22 09:11:29 +0700

    commit

--- model/connect/connection.php
+++ model/connect/connection.php
@@ -1,7 +1,5 @@
 <?php 
-$hostInfo = 'mysql:host=127.0.0.1:3306; dbname=albumdb; charset=utf8mb4';
-$userAdmin = 'root';
-$passAdmin = 'Teotu_19';
+require_once 'dbConfigVar.php' ;
 
 try {
     $pdo = new PDO($hostInfo, $userAdmin, $passAdmin);
---
+++ model/connect/dbConfigVar.php
@@ -0,0 +1,5 @@
+<?php 
+$hostInfo = 'mysql:host=127.0.0.1:3306; dbname=albumdb; charset=utf8mb4';
+$userAdmin = 'root';
+$passAdmin = 'Teotu_19';
+?>
